3b6254fb9288908b0052b39106bc6cf0124a157a
MAAT <MAAT@PROELE58>
2025-05-23 08:36:55 +0200

    xd

---
+++ Calender/Controllers/AuthController.cs
@@ -0,0 +1,102 @@
+﻿using Calender.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calender.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class AuthController : ControllerBase
+    {
+        private readonly DatabaseContext _context;
+        IConfiguration configuration;
+        public AuthController(DatabaseContext context, IConfiguration configuration)
+        {
+            this._context = context;
+            this.configuration = configuration;
+        }
+
+        [HttpPost("register")]
+
+        public async Task<ActionResult<User>> Register(User request)
+        {
+            if (string.IsNullOrWhiteSpace(request.Username) || string.IsNullOrWhiteSpace(request.PasswordHash))
+                return BadRequest("Username and password are required");
+
+            if (await _context.Users.AnyAsync(u => u.Username == request.Username))
+                return BadRequest("Username already exists.");
+
+
+            var user = new User
+            {
+                Username = request.Username
+            };
+
+            var passwordHasher = new PasswordHasher<User>();
+            user.PasswordHash = passwordHasher.HashPassword(user, request.PasswordHash);
+
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return Ok(new { user.UserId, user.Username });
+        }
+
+        [HttpPost("login")]
+
+        public async Task<ActionResult<User>> login(User request)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.Username == request.Username);
+
+            if (user == null)
+                return BadRequest("User or password is incorrect");
+
+            var hasher = new PasswordHasher<User>();
+            var result = hasher.VerifyHashedPassword(user, user.PasswordHash, request.PasswordHash);
+
+            if (result == PasswordVerificationResult.Failed)
+                return BadRequest("User or password is incorrect");
+
+            var token = CreateToken(user);
+            return Ok(token);
+        }
+
+        [Authorize]
+        [HttpGet]
+        public IActionResult AuthenticatedOnlyEndpoint()
+        {
+            return Ok("You are authenticated!");
+        }
+
+        private string CreateToken(User user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, user.Username),
+                new Claim(ClaimTypes.NameIdentifier, user.UserId.ToString())
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JwtSettings:Token"]!));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512);
+
+            var token = new JwtSecurityToken(
+                issuer: configuration["JwtSettings:Issuer"],
+                audience: configuration["JwtSettings:Audience"],
+                claims: claims,
+                expires: DateTime.UtcNow.AddMinutes(configuration.GetValue<int>("JwtSettings:ExpireMinutes")),
+                signingCredentials: creds
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
--- Calender/Program.cs
+++ Calender/Program.cs
@@ -1,8 +1,11 @@
 using Calender.Interface;
 using Calender.Models;
 using Calender.Repositories;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using Scalar.AspNetCore;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -18,11 +21,24 @@ builder.Services.AddScoped<IUser, UserRepo>();
 
 
 
-
-
-
 string connectionstring = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<DatabaseContext>(options => options.UseSqlServer(connectionstring, b => b.MigrationsAssembly("Calender")));
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidIssuer = builder.Configuration["JwTSettings:Issuer"],
+            ValidateAudience = true,
+            ValidAudience = builder.Configuration["JwTSettings:Audience"],
+            ValidateLifetime = true,
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(builder.Configuration["JwTSettings:Token"]!)),
+            ValidateIssuerSigningKey = true
+        };
+    });
 
 
 var app = builder.Build();
